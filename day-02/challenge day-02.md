c0255c5b92cc8b4d0dda09adc46d91f5e984cb69
María LC <15439195+marialc@users.noreply.github.com>
2024-11-13 09:45:55 +0100

    day 2 complete

---
+++ day-02/challenge day-02.md
@@ -0,0 +1,3 @@
+1. Calculate total sales for each month from the Invoice table:
+  Tables Involved: Invoice
+  Output Columns: Month, TotalSales
---
+++ day-02/query.sql
@@ -0,0 +1,6 @@
+SELECT 
+    strftime('%Y-%m', "InvoiceDate") AS "Month",
+    SUM("Total") AS "TotalSales" 
+FROM Invoice
+GROUP BY "Month"
+ORDER BY "Month";
