5748dbb728d9edc364c61e5950345b8404f6339b
María LC <15439195+marialc@users.noreply.github.com>
2024-11-25 18:06:10 +0100

    day 14 complete

---
+++ day-14/challenge.md
@@ -0,0 +1,3 @@
+1. Show first name, last name, and the full province name of each patient  
+
+Example: 'Ontario' instead of 'ON'. Your 3rd column should be named "province_name"
---
+++ day-14/query.sql
@@ -0,0 +1,7 @@
+SELECT
+    patients.first_name,
+    patients.last_name,
+    province_names.province_name
+FROM patients
+JOIN province_names
+ON province_names.province_id = patients.province_id;
